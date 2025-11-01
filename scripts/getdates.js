494132ba8dd5d7214fe306ec9077d0598f9e817c
g10albert <albertmiguelg10@gmail.com>
2025-11-01 13:43:34 -0400

    Created and styled the homepage

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+const today = new Date();
+
+document.getElementById("currentYear").innerHTML = today.getFullYear();
+
+document.getElementById("lastModified").innerHTML = `Last modification: ${document.lastModified}`;
