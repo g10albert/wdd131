014bcbf8656b93d778ba212fe67c40dea16414b0
g10albert <albertmiguelg10@gmail.com>
2025-11-07 00:26:41 -0400

    Finished the menu and adapted it to large screens. Finished the grid and added the photos

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,1 +1,13 @@
 document.getElementById("lastModified").innerHTML = `Last modification: ${document.lastModified}`;
+
+const menuBtn = document.querySelector("#menuBtn")
+const menuCloseBtn = document.querySelector("#menuCloseBtn")
+const menu = document.querySelector("#menu")
+
+menuBtn.addEventListener('click', () => {
+  menu.classList.add("menu_open")
+})
+
+menuCloseBtn.addEventListener("click", () => {
+  menu.classList.remove("menu_open")
+})
